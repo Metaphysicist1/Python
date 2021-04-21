5857a5bd2a7bc4d496327ceea066c83eac5a5b7f
X-Knight-X <edgari.edgari@mail.ru>
2021-04-22 00:04:27 +0400

    E

---
+++ Data Structures/queue.py
@@ -0,0 +1,24 @@
+#queue.py not built in data structure for specialized requirments
+class queue:
+    def __init__(self):
+        self.data = []
+    
+    def enqueue(self,el):
+        self.data.insert(0,el)
+    
+    def dequeue(self):
+        self.data.pop()
+    
+    def print_queue(self):
+        for el in self.data: 
+            print(el, end=' --> ')
+
+a = queue()
+a.enqueue(3)
+a.enqueue(4)
+a.enqueue(45)
+a.enqueue(123)
+a.print_queue()
+print('\n')
+a.dequeue()
+a.print_queue()

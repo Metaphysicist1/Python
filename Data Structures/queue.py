b76cd97d9144e94ac1b94a305c679af87cf62a21
X-Knight-X <edgari.edgari@mail.ru>
2021-04-25 01:18:24 +0400

    master

--- Data Structures/queue.py
+++ Data Structures/queue.py
@@ -1,17 +1,23 @@
 #queue.py not built in data structure for specialized requirments
 class queue:
+    #constructor
     def __init__(self):
         self.data = []
-
+    #printing
     def __str__(self):
         for i in self.data:
             print ('{}'.format(i),end=' --> ')
-    
+    # add element
     def enqueue(self,el):
         self.data.insert(0,el)
-    
+    # delete element
     def dequeue(self):
         self.data.pop()
+    # queue's size
+    def length(self):
+        print(len(self.data))
+
+    
     
     
 
@@ -23,4 +29,4 @@ a.enqueue(123)
 a.__str__()
 print('\n')
 a.dequeue()
-#a.print_queue()
+a.length()

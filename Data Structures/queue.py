ccd712569f56d0ed5c4c0f711ddc66612b6dcb73
X-Knight-X <edgari.edgari@mail.ru>
2021-04-25 13:54:53 +0400

    master

--- Data Structures/queue.py
+++ Data Structures/queue.py
@@ -1,31 +1,43 @@
 #queue.py not built in data structure for specialized requirments
-class queue:
-    #constructor
+class Node:
+    def __init__(self,value):
+        self.value = value
+
+class Queue:
+    #constructor initialization
     def __init__(self):
         self.data = []
-    #printing
+    #printing queue
     def __str__(self):
         for i in self.data:
             print ('{}'.format(i),end=' --> ')
-    # add element
-    def enqueue(self,el):
-        self.data.insert(0,el)
-    # delete element
+    # add element to queue
+    def enqueue(self,value):
+        self.data.insert(0,Node(value))
+    # delete element from queue
     def dequeue(self):
         self.data.pop()
     # queue's size
     def length(self):
         print(len(self.data))
+    #setter
+    def setter(self,new):
+        self.data= new
+    #getter
+    def getter(self):
+        return self.data
+    
 
     
     
     
 
-a = queue()
-a.enqueue(3)
-a.enqueue(4)
-a.enqueue(45)
-a.enqueue(123)
+a = Queue()
+
+a.enqueue(Node(3))
+a.enqueue(Node(4))
+a.enqueue(Node(45))
+a.enqueue(Node(123))
 a.__str__()
 print('\n')
 a.dequeue()
